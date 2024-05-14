c6b34f71d4c62e8374ebdbb72a01cec7c96e2b6b
Abdulmajid Ramadan Hassan <45086070+Sudan1841@users.noreply.github.com>
2024-05-14 03:06:10 -0600

    Update firebaseConfig.js
    this some change in firebaseConfig 
    and add you firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,28 +1,17 @@
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore';
-
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import firebase from 'firebase/app';
+import 'firebase/database'; // Import other Firebase services as needed
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfLzg5Aeua0dA2TeWRAgslgyBXAeIzows",
-  authDomain: "my-project-1562563201095.firebaseapp.com",
-  databaseURL: "https://my-project-1562563201095.firebaseio.com",
-  projectId: "my-project-1562563201095",
-  storageBucket: "my-project-1562563201095.appspot.com",
-  messagingSenderId: "212338060009",
-  appId: "1:212338060009:web:09ba642ee65882b2e65e78",
-  measurementId: "G-S6PR5971TG"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-export { app, analytics, db };
+firebase.initializeApp(firebaseConfig);
 
+export default firebase;
